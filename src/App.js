6b796079dfefa0f555bb2eed950e58e2f1b0a57e
hang5607 <hang5607@naver.com>
2021-01-30 14:44:16 +0900

    Material UI 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customers';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
 
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+});
 const customers = [
   {
   'id': 1,
@@ -23,22 +39,29 @@ const customers = [
 
 class App extends Component{
   render(){
+    const { classes } = this.props;
     return(
-      <div>
-        {customers.map(c => {
-          return <Customer
-          id={c.id}
-          name={c.name}
-          phoneNumber={c.phoneNumber}
-          doctor={c.doctor}
-          birthday={c.birthday}
-          visitDay={c.visitDay}
-        />
-        })}
+      <div className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>환자명</TableCell>
+              <TableCell>핸드폰번호</TableCell>
+              <TableCell>등록자</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>방문날짜</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {
+            return <Customer key={c.id} id={c.id} name={c.name} phoneNumber={c.phoneNumber} doctor={c.doctor} birthday={c.birthday} visitDay={c.visitDay} />
+            })}
+          </TableBody>
+        </Table>
       </div>
-      
     )
   }
 };
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component{
     render(){
         return(
-            <div>
-                <h2>{this.props.id}</h2>
-                <h2>{this.props.name}</h2>
-                <h2>{this.props.phoneNumber}</h2>
-                <h2>{this.props.doctor}</h2>
-                <h2>{this.props.birthday}</h2>
-                <h2>{this.props.visitDay}</h2>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.phoneNumber}</TableCell>
+                <TableCell>{this.props.doctor}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.visitDay}</TableCell>
+            </TableRow>
         )
     }
 }
